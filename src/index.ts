ac4390a72b2d91c6819a59ad12604d9add552432
Mushy <k3.arjun@gmail.com>
2025-02-14 13:53:10 +1100

    sum shi idk i forgot

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,28 @@
-import { snipe } from 'deckspyinator';
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
-import screenshot from 'screenshot-desktop';
-import sharp from 'sharp';
-import { createWorker } from 'tesseract.js';
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
 
-const key = readFileSync(join(__dirname, '../dev/key.txt')).toString();
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
-(async () => {
-    const sc = await screenshot({ format: 'png' });
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-    writeFileSync(join(__dirname, '../data/capture.png'), sc);
-    // const sc = join(__dirname, '../data/capture.png');
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, '../pages/index.html'));
+});
 
-    const img = await sharp(sc)
-        .extract({ left: 47, top: 33, height: 33 + 25, width: 221 })
-        .toBuffer();
+io.on('connection', socket => {
+    console.log('a user connected');
 
-    writeFileSync(join(__dirname, '../data/player.png'), img);
+    socket.on('chat message', msg => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
 
-    const clanImg = await sharp(img)
-        .extract({ left: 0, top: 34, width: 221, height: 24 })
-        .grayscale()
-        .normalise({ lower: 80, upper: 100 })
-        .negate({ alpha: false })
-        .resize({ height: 100 })
-        .modulate({
-            brightness: 0.5,
-        })
-        .toBuffer();
-
-    const playerImg = await sharp(img)
-        .extract({ top: 0, left: 2, width: 219, height: 33 })
-        .resize({ height: 100 })
-        .normalise({ lower: 60, upper: 90 })
-        .modulate({
-            brightness: 0.3,
-        })
-        .toBuffer();
-
-    writeFileSync(join(__dirname, '../data/clanPP.png'), clanImg);
-    writeFileSync(join(__dirname, '../data/playerPP.png'), playerImg);
-
-    const worker = await createWorker('eng');
-
-    const {
-        data: { text: clan },
-    } = await worker.recognize(clanImg);
-
-    console.log('Clan: ' + clan);
-
-    const {
-        data: { text: player },
-    } = await worker.recognize(playerImg);
-
-    console.log('Player: ' + player);
-
-    worker.terminate();
-
-    snipe(key, player, clan, true);
-})();
+server.listen(3000, () => {
+    console.log('server running at http://localhost:3000');
+});
